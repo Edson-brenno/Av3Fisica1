39fb61c0acfe3f572b15ae1f25eb61a83217066d
brenno <brennorebeiro@gmail.com>
2024-06-30 19:20:39 -0300

    Reconfigura mensagens

--- main.py
+++ main.py
@@ -4,16 +4,14 @@ import vpython as vp
 if __name__ == '__main__':
     
     sistema = VpythonManager()
-
-    sistema.adicionar_titulo("Av3 Fisica 1")
     
-    sistema.adicionar_titulo("O Problema: ")
+    sistema.adicionar_titulo("Av3 Fisica 1", caption="""<hr>
+<h1> O Problema: </h1> 
+    <strong> De t = 0 a t = 5,00 min </strong>, um homem fica em pé sem se mover; de <b>t = 5,00 min a t = 10,0 min</b>, caminha em linha reta com uma velocidade de 2,2 m/s.
     
-    sistema.adicionar_texto("""De t = 0 a t = 5,00 min, um homem fica em pé sem se mover; de t
-    = 5,00 min a t = 10,0 min, caminha em linha reta com uma
-    velocidade de 2,2 m/s.
+    <b>Qual é (a) a velocidade média vméd e (b) qual a aceleração média améd do homem no intervalo de tempo de 2,00 min a 8,00 min?</b>
     """)
     
-    sistema.adicionar_titulo("teste")
+    sistema.adicionar_titulo("a")
     # Display text input prompt in the console
     user_input = input("Enter something: ")
--- utilitarios/vpython_manager.py
+++ utilitarios/vpython_manager.py
@@ -23,16 +23,32 @@ class VpythonManager():
 
         self._posicao_y -= 4
     
-    def adicionar_titulo(self, titulo : str, cor=vp.color.black) -> None:
+    def adicionar_titulo(self, titulo : str, cor=vp.color.black, caption = "") -> None:
 
         t = vp.canvas()
         # Set the scene properties
         t.background = vp.color.white
         t.center = vp.vector(0, 0, 0)  # Centering the scene to show both objects
         t.autoscale = True
+        t.caption = caption
 
 
         novo_titulo = vp.text(text=titulo, align='center', color=cor, pos= vp.vector(0, 0, 0), height=1, canvas=t)
+        
+        self.__acrescer_posicao_y()
+    
+    def adicionar_titulo_com_texto(self, titulo : str, cor=vp.color.black, descricao="") -> None:
+
+        t = vp.canvas()
+        # Set the scene properties
+        t.background = None #vp.color.white
+        t.center = vp.vector(0, 0, 0)  # Centering the scene to show both objects
+        t.autoscale = True
+
+
+        novo_titulo = vp.text(text=titulo, align='center', color=cor, pos= vp.vector(0, 0, 0), height=1, canvas=t)
+        
+        wtext = vp.wtext(text=descricao, background= vp.color.white)
         
         self.__acrescer_posicao_y()
 
@@ -42,4 +58,11 @@ class VpythonManager():
         novaCena.center = vp.vector(0, 0, 0)
         
         wtext = vp.text(text=texto, color=cor, height= 1, align='center', canvas=novaCena, pos= vp.vector(0, 0, 0))
+        
+    def adicionar_wtexto(self, texto : str, cor=vp.color.black):
+        
+        novaCena = vp.canvas(background= vp.color.white, autoscale = True)
+        novaCena.center = vp.vector(0, 0, 0)
+        
+        wtext = vp.wtext(text=texto, color=cor, height= 1, align='center', canvas=novaCena, pos= vp.vector(0, 0, 0))
         
